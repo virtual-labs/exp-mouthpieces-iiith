a1190471a969f00c22b310a90a0a0bd66a3738fc
vjspranav <pranavasri@live.in>
2021-03-17 18:23:44 +0530

    responsiveness increased * Also Add slider to control animation speed

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,77 +1,79 @@
 let sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-  
-  let arrayRemove = (arr, value) => {
-    return arr.filter(function (ele) {
-      return ele != value;
-    });
-  };
-  
-  /**
-   * tube-1
-   * tube-2
-   */
-  ids = [
-    "long-1",
-    "container-1",
-    "curve-start",
-    "curve",
-    "long-2",
-    "container-2"
-  ];
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-  let asyncMove = async (id, curPosition = 0, finalPosition = 1) => {
+let arrayRemove = (arr, value) => {
+  return arr.filter(function (ele) {
+    return ele != value;
+  });
+};
+
+/**
+ * tube-1
+ * tube-2
+ */
+ids = [
+  "long-1",
+  "container-1",
+  "curve-start",
+  "curve",
+  "long-2",
+  "container-2",
+];
+
+let asyncMove = async (id, curPosition = 0, finalPosition = 1) => {
+  let path = document.getElementById(id);
+  let flags = [true, true, true, true, true, true, true];
+  while (true) {
+    speed2 = document.getElementById("water-flow").value;
+    speed2 = speed2 * 0.0002;
+    speed2 = speed2 == 0 ? 0.0002 : speed2;
+    // For Moving water in all seven pipes asynchronusly
+    if (curPosition > finalPosition) break;
+    curPosition += speed2;
+    path.setAttribute("offset", curPosition);
+    await sleep(0.5);
+  }
+};
+
+let startAnimation = async () => {
+  let flags = [1, 1, 1, 1];
+  for (let i = 0; i < ids.length; i++) {
+    speed2 = document.getElementById("water-flow").value;
+    speed2 = speed2 * 0.0002;
+    speed2 = speed2 == 0 ? 0.0002 : speed2;
+    id = ids[i];
     let path = document.getElementById(id);
-    let flags = [true, true, true, true, true, true, true];
+    let finalPosition = 1;
+    let curPosition = 0;
     while (true) {
-      // For Moving water in all seven pipes asynchronusly
       if (curPosition > finalPosition) break;
-      curPosition += 0.01;
+      curPosition += speed2;
       path.setAttribute("offset", curPosition);
-      await sleep(0.5);
+      await sleep(2);
     }
-  };
-  let startAnimation = async () => {
-    let flags = [1, 1, 1, 1]
-    for (let i = 0; i < ids.length; i++) {
-      id = ids[i];
-      let path = document.getElementById(id);
-      let finalPosition = 1;
-      let curPosition = 0;
-      while (true) {    
-        if (curPosition > finalPosition) break;
-        curPosition += 0.01;
-        path.setAttribute("offset", curPosition);
-        await sleep(2);
-      }
-    }
-  };
-  
-  let resetEverything = () => {
-    tube_ids = [
-      // null
-    ];
-    tube_ids.forEach((element) => {
-      let path = document.getElementById(element);
-      path.setAttribute("offset", 0);
-    });
-    ids.forEach((ele) => {
-      let path = document.getElementById(ele);
-      path.setAttribute("offset", 0);
-    });
-  };
-  disablestart = false;
-  let startAn = async () => {
-    resetEverything();
-    document.getElementById("startbutton").className = "button disabled";
-    document.getElementById("resetbutton").className = "button disabled";
-    document.getElementById("startbutton").disabled = true;
-    document.getElementById("resetbutton").disabled = true;
-    await startAnimation();
-    document.getElementById("startbutton").className = "button";
-    document.getElementById("resetbutton").className = "button";
-    document.getElementById("startbutton").disabled = false;
-    document.getElementById("resetbutton").disabled = false;
-  };
-  
+  }
+};
+
+let resetEverything = () => {
+  tube_ids = [
+    // null
+  ];
+  tube_ids.forEach((element) => {
+    let path = document.getElementById(element);
+    path.setAttribute("offset", 0);
+  });
+  ids.forEach((ele) => {
+    let path = document.getElementById(ele);
+    path.setAttribute("offset", 0);
+  });
+};
+disablestart = false;
+let startAn = async () => {
+  resetEverything();
+  document.getElementById("startbutton").disabled = true;
+  document.getElementById("resetbutton").disabled = true;
+  await startAnimation();
+  document.getElementById("startbutton").disabled = false;
+  document.getElementById("resetbutton").disabled = false;
+};
